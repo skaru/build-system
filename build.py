c759af83eee57b6ff66d5b6e8ea044adcd1f4ed0
skaru <4243666+skaru@users.noreply.github.com>
2020-09-09 13:57:37 +0200

    Added build script

---
+++ build.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+import os
+import sys
+import shutil
+
+def main():
+    parser = argparse.ArgumentParser(description='build.py - Build Tool', prog=os.path.basename(sys.argv[0]))
+    parser.add_argument("-b", "--build", action="store_true", help="Build project")
+    parser.add_argument("-c", "--clean", action="store_true", help="Clean project")
+    parser.add_argument("-m","--menuconfig", action="store_true", help="Run menuconfig")
+    args = parser.parse_args()
+
+    if args.clean == True:
+        shutil.rmtree("build")
+    else:
+        if not os.path.exists('build'):
+            os.makedirs('build')
+        
+        if args.menuconfig == True:    
+            os.environ["KCONFIG_CONFIG"] = "build/.config"
+            subprocess.call("python tools/menuconfig.py", env=os.environ.copy())
+        elif args.build == True:
+            if not os.path.exists('build/.config'):
+                os.environ["KCONFIG_CONFIG"] = "build/.config"
+                subprocess.call("python tools/menuconfig.py", env=os.environ.copy())
+            subprocess.call("python tools/confgen.py --kconfig Kconfig --config build/.config --output header build/config.h --output cmake build/config.cmake")
+            os.chdir("build")
+            subprocess.call("cmake ..")
+            subprocess.call("cmake --build .")
+
+if __name__ == "__main__":
+    main()
